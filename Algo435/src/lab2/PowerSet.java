a1bd7bc9b6c36931d996fb52c8625c3de37c1589
Muluadam Temesgen Akalu <mtmuluadam08@gmail.com>
2021-09-02 08:19:58 -0500

    powerset with generics

--- Algo435/src/lab2/PowerSet.java
+++ Algo435/src/lab2/PowerSet.java
@@ -1,1 +1,46 @@
+package lab2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PowerSet {
+    public static <T> List<Set<T>> powerSet(List<T> X) {
+        List<Set<T>> P = new ArrayList<Set<T>>();
+        Set<T> S = new HashSet<T>();
+        P.add(S);
+        if(X.isEmpty()) {
+            return P;
+        }
+        else {
+            while(!X.isEmpty()) {
+                T f = X.remove(0);
+                int size = P.size();
+                for(int i = 0; i < size; i++) {
+                    S = new HashSet<T>();
+                    S.add(f);
+                    S.addAll(P.get(i));
+                    P.add(S);
+                }
+            }
+        }
+        return P;
+    }
+
+    public static void main(String[] args) {
+        int[] a={2,4,6,1};
+        List<Integer> nums=new ArrayList<>();
+        nums.add(2);
+        nums.add(4);
+        nums.add(7);
+        List<String>names=new ArrayList<>();
+        names.add("Dove");
+        names.add("Aman");
+        names.add("Max");
+
+
+        System.out.println(powerSet(nums));
+        System.out.println(powerSet(names));
+    }
+}
